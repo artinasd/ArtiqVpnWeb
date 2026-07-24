2fd8f5d0b51bdb6797e6d24c7190def35ad68831
Artin <am8.artin@gmail.com>
2026-07-24 12:22:18 +0330

    .

--- artiqvpnweb/vite.config.js
+++ artiqvpnweb/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/artiqvpnweb/', // IMPORTANT: Make sure this matches your repo name
   plugins: [
     react(),
     tailwindcss(),
